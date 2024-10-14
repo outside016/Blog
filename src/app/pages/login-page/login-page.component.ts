1b612ebd33156127ee1d89669e422bdfa01c0ab5
outside016 <outside016@icloud.com>
2024-10-14 21:26:19 +0300

    card d2 player

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
-import {FormsModule} from "@angular/forms";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+
+  form = new FormGroup({
+    username: new FormControl(null),
+    password: new FormControl(null),
+  })
+
+  onSubmit(){
+    console.log(this.form.value)
+  }
 
 }
