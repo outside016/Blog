b58253e85afd95aca1bc6ada51c0ef8b60de5b71
outside016 <outside016@icloud.com>
2024-10-23 20:01:18 +0300

    card d2 player

--- src/app/comp-ref/sidebar/sidebar.component.ts
+++ src/app/comp-ref/sidebar/sidebar.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {SvgIconComponent} from "../svg-icon/svg-icon.component";
+import {SubscriberCardComponent} from "./subscriber-card/subscriber-card.component";
+import {RouterLink} from "@angular/router";
+import {ProfileService} from "../../data/services/profile.service";
+import {AsyncPipe, JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
-    SvgIconComponent
+    SvgIconComponent,
+    SubscriberCardComponent,
+    RouterLink,
+    AsyncPipe,
+    JsonPipe
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+
+  profileService= inject(ProfileService)
+
+  subscribers$ = this.profileService.getSubscribersShortList()
 
   menuItems = [
     {
--- src/app/data/services/profile.service.ts
+++ src/app/data/services/profile.service.ts
@@ -1,6 +1,8 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Profile} from "../interface/profile.interface";
+import {PagebleInterface} from "../interface/pageble.interface";
+import {map} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +15,13 @@ export class ProfileService {
 
   getTestAccounts(){
     return this.http.get<Profile[]>(`${this.baseApiUrl}account/test_accounts`)
+  }
+
+  getSubscribersShortList(){
+    return this.http.get<PagebleInterface<Profile>>(`${this.baseApiUrl}account/subscribers/`)
+      .pipe(
+      map(res => res.items.slice(0,3))
+      )
   }
 
   getMe(){
