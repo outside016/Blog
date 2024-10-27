8837ddfc08a5d72090bb2531be316781fb022972
outside016 <outside016@icloud.com>
2024-10-27 18:08:35 +0300

    card d2 player

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,17 +1,26 @@
 import {Component, inject} from '@angular/core';
 import {ProfileHeaderComponent} from "../../comp-ref/profile-header/profile-header.component";
 import {ProfileService} from "../../data/services/profile.service";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, RouterLink} from "@angular/router";
 import {switchMap} from "rxjs";
 import {toObservable} from "@angular/core/rxjs-interop";
 import {AsyncPipe} from "@angular/common";
+import {SvgIconComponent} from "../../comp-ref/svg-icon/svg-icon.component";
+import {SubscriberCardComponent} from "../../comp-ref/sidebar/subscriber-card/subscriber-card.component";
+import {ImgUrlPipe} from "../../helpers/pipes/img-url.pipe";
+import {PostFeedComponent} from "./post-feed/post-feed.component";
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
   imports: [
     ProfileHeaderComponent,
-    AsyncPipe
+    AsyncPipe,
+    RouterLink,
+    SvgIconComponent,
+    SubscriberCardComponent,
+    ImgUrlPipe,
+    PostFeedComponent
   ],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss'
@@ -23,6 +32,8 @@ export class ProfilePageComponent {
   route = inject(ActivatedRoute);
 
   me$ = toObservable(this.profileService.me)
+
+  subscribers$ = this.profileService.getSubscribersShortList(5);
 
   profile$ = this.route.params
     .pipe(
