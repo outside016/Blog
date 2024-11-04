4fc1bed24b4b352508b47cea96e89e336539d8d7
outside016 <outside016@icloud.com>
2024-11-04 16:18:48 +0300

    card d2 player

--- src/app/pages/search-page/profile-filters/profile-filters.component.ts
+++ src/app/pages/search-page/profile-filters/profile-filters.component.ts
@@ -1,16 +1,47 @@
-import { Component } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {Component, inject, OnDestroy} from '@angular/core';
+import {FormBuilder, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ProfileService} from "../../../data/services/profile.service";
+import {debounceTime, startWith, Subscription, switchMap} from "rxjs";
 
 @Component({
   selector: 'app-profile-filters',
   standalone: true,
-    imports: [
-        FormsModule,
-        ReactiveFormsModule
-    ],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './profile-filters.component.html',
   styleUrl: './profile-filters.component.scss'
 })
-export class ProfileFiltersComponent {
+export class ProfileFiltersComponent implements OnDestroy {
+
+  profileService = inject(ProfileService)
+
+  fb = inject(FormBuilder)
+
+
+  searchForm = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    stack: ['']
+  })
+
+  searchFormSub!: Subscription;
+
+  constructor() {
+    this.searchFormSub = this.searchForm.valueChanges
+      .pipe(
+        startWith({}),
+        debounceTime(300),
+        switchMap(formValue => {
+          return this.profileService.filterProfiles(formValue)
+        })
+      )
+      .subscribe()
+  }
+
+  ngOnDestroy() {
+    this.searchFormSub.unsubscribe()
+  }
 
 }
--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -3,13 +3,15 @@ import {ProfileCardComponent} from "../../comp-ref/profile-card/profile-card.com
 import {ProfileService} from "../../data/services/profile.service";
 import {Profile} from "../../data/interface/profile.interface";
 import {ProfileFiltersComponent} from "./profile-filters/profile-filters.component";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-search-page',
   standalone: true,
   imports: [
     ProfileCardComponent,
-    ProfileFiltersComponent
+    ProfileFiltersComponent,
+    AsyncPipe
   ],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss'
@@ -18,12 +20,9 @@ export class SearchPageComponent {
 
   profileService = inject(ProfileService);
 
-  profiles: Profile[] = [];
+  profiles = this.profileService.filteredProfiles
 
   constructor() {
-    this.profileService.getTestAccounts()
-      .subscribe(val => {
-        this.profiles = val
-      })
+
   }
 }
