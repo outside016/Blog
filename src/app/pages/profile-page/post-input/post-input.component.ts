053f9c5c7d1ebb0aa2a4d7ea102d822b03b3ac24
outside016 <outside016@icloud.com>
2024-11-06 21:31:33 +0300

    card d2 player

--- src/app/pages/profile-page/post-input/post-input.component.ts
+++ src/app/pages/profile-page/post-input/post-input.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, Renderer2} from '@angular/core';
 import {AvatarCircleComponent} from "../../../comp-ref/avatar-circle/avatar-circle.component";
 import {ProfileService} from "../../../data/services/profile.service";
 import {NgIf} from "@angular/common";
@@ -14,5 +14,12 @@ import {NgIf} from "@angular/common";
   styleUrl: './post-input.component.scss'
 })
 export class PostInputComponent {
-profile = inject(ProfileService).me
+  r2 = inject(Renderer2)
+  profile = inject(ProfileService).me
+
+  onTextAreaInput(event: Event) {
+    const textarea = event.target as HTMLTextAreaElement;
+
+    this.r2.setStyle(textarea, 'height', textarea.scrollHeight + 'px');
+  }
 }
