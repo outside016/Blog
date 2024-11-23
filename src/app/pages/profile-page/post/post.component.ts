889517e260902b23ee37eb5c93b23693a3f1bacf
outside016 <outside016@icloud.com>
2024-11-23 19:01:45 +0300

    card d2 player

--- src/app/pages/profile-page/post/post.component.ts
+++ src/app/pages/profile-page/post/post.component.ts
@@ -3,13 +3,15 @@ import {Post} from "../../../data/interface/post.interface";
 import {AvatarCircleComponent} from "../../../comp-ref/avatar-circle/avatar-circle.component";
 import {PostService} from "../../../data/services/post.service";
 import {DatePipe} from "@angular/common";
+import {SvgIconComponent} from "../../../comp-ref/svg-icon/svg-icon.component";
 
 @Component({
   selector: 'app-post',
   standalone: true,
   imports: [
     AvatarCircleComponent,
-    DatePipe
+    DatePipe,
+    SvgIconComponent
   ],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
